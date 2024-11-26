39d490a8f34320a269935cfa41a527fa351f1033
gb585 <gorka_bon@hotmail.com>
2024-11-26 23:51:56 +0100

    versión_261124_app

--- app.py
+++ app.py
@@ -14,15 +14,9 @@ with open('scaler_regres_logistica.pkl', 'rb') as scaler_file:
 st.title('Predicción de si un cliente contratará (1) o no un depósito bancariao (Regresión Logística)')
 
 # Entrada de datos del usuario
-
 housing = int(st.selectbox('¿Tiene hipoteca? (1: Sí, 0: No)',options=[1, 0]))
 balance=float(st.number_input('¿Cuál es el saldo promedio anual en la cuenta bancaria?'))
 cat_poutcome_success= int(st.selectbox('¿Contrató productos en campañas anteriores? (1: Sí, 0: No)',options=[1, 0]))
-
-#housing = 1
-#balance = 100
-#cat_poutcome_success = 0
-
 
 # Crear un DataFrame con las entradas
 user_data = pd.DataFrame({
@@ -31,21 +25,16 @@ user_data = pd.DataFrame({
      'cat_poutcome_success': [cat_poutcome_success]
  })
 
-
-# Estandarizar las entradas. Para entrar un único valor np.array tiene que ser bidimensional, por por eso reshape(1,-1)
-#También serviría balance_standarized = scaler.transform(np.array([[balance]]))
-#Si vull fer una entrada com una única columna, tinc que fer reshape a part de fer values, per tenir un array numpy.
-
-#Para un único escalar que se le pase a scaler.transform debo recuperar primero los valores de user_data['balance] con user_data['balance'].values
-# y luego hacer reshape para que tenga dos dimensiones.
+#Para un único valor numérico que se le pase a scaler.transform debo recuperar primero los valores de user_data['balance] con user_data['balance'].values
+# y luego hacer reshape(-1,1) para que tenga dos dimensiones tal como necesita scaler.transform
 balance_np=user_data['balance'].values.reshape(1,-1)
 balance_standarized = scaler.transform(balance_np)
 
 #Canvio el valor de balance del DataFrame por el estandarizado
 user_data['balance']=balance_standarized 
 
-# Crear un array de 1 fila y 3 columnas. Crear un numpy array a partir de pandas és importante para poder utilizar el model.predict. Y así el modelo no contendrá
-#las nombres de las columnas del DataFrame de pandas.
+# Cuando cree el modelo (modelo.fit) le pase un numpy array de las características a entrenar para no tenr conflicos de los nombres de columna. 
+#Por este motivo, ahora le dobo pasar un numpy array utilizando el atributo values
 
 data = user_data.values
 # Realizar la predicción
@@ -53,5 +42,5 @@ y_pred = model.predict(data)
 
 # Mostrar la predicción
 st.write(f'Predicción de la contratación del depósito: {y_pred[0]}')
-#print(y_pred[0])
+
 
